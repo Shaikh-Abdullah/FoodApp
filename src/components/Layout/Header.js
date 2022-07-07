bf18a348365977802d42e1e6c55bd76ca0b43189
Abdullah <shabdullah685@gmail.com>
2022-07-07 18:41:54 +0530

    Add meal section

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 
 import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from '../../assets/spencer-davis-5UeN8VrCxvs-unsplash.jpg'
 
 const Header = props => {
@@ -8,7 +9,7 @@ const Header = props => {
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="Table full of Meals" />
